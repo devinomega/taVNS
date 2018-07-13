74c41c9d3d08f804f6da8e4a6caaaa21204e37c6
devinDev <neuroccny@gmail.com>
2018-07-13 14:45:32 -0400

    update Jake Pipeline to cur dir

--- ECG_Pipeline_Jake.m
+++ ECG_Pipeline_Jake.m
@@ -5,22 +5,18 @@
 % 3) Peak Pick
 
 %% PreProcessing
+%
 updateTables
 
 updateECG
 
-%% ECG
-%heart rate picking for the five blocks before during and after stim
-ECG_preProc('skip',false,'startName','TV_20_02')
-% heart rate picking for the baseline 2.5 min
-ECGbaselinePreProc('skip',false,'startName','TV_27_04')
-%heart rate picking for the five blocks post number rating
-ECGpostPreProc('skip',false,'startName','TV_02_01')
-%% Resp
-% resperation picking for the five blocks before during and after stim
-breath_preProc('skip',false,'startName','TV_20_02')
-% resp rate picking for the baseline 2.5 min
-breathBaselinePreProc('skip',false,'startName','TV_27_08')
+ECG_preProc('filePath', 'C:\Users\bikson\Desktop\taVNS scripts\subjData\', 'skip',false,'startName','TV_11_07')
+%% heart rate picking for the five blocks^
+breath_preProc('filePath', 'C:\Users\bikson\Desktop\taVNS scripts\subjData\', 'skip',false,'startName','TV_01_01')
+%%resperation picking for the five blocks^
+ECGbaselinePreProc('filePath', 'C:\Users\bikson\Desktop\taVNS scripts\subjData\', 'skip',false,'startName','TV_27_04')
+%% heart rate for baseline^
+breathBaselinePreProc('filePath', 'C:\Users\bikson\Desktop\taVNS scripts\subjData\', 'skip',false,'startName','TV_27_08')
 %% Run Stats
 
-HR_Stats()
+Hr_Stats()
